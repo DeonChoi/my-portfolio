e4945ccb67b83f570482e8c665793b39c30ece39
Deon Choi <deon.choi@gmail.com>
2019-10-21 23:03:02 -0700

    4th Update
    Updated Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,18 +3,22 @@ import JokeCardsImage from '../images/JokeCards.png';
 import RandomQuoteMachineImage from '../images/RandomQuoteMachine.png';
 import WeatherImage from '../images/Weather.png';
 import GitHubIcon from '../images/github_light.png';
+import TicTacToeImage from '../images/TicTacToe.png';
 import { Row, Col, Badge } from 'react-bootstrap';
 
 class Projects extends React.Component {
     render () {
         return (
-            <Col id='projects'>
+            <Row id='projects' className='w-100'>
+                
                 <div className='overlay-layer'></div>
-
-                <Row className='d-flex justify-content-end project-container'>
-
-                    <Row>
-                        <p className='project-title font-weight-bold'>Projects</p>
+                <Col className='d-flex flex-column project-container primary'>
+                    
+                    <Row className='justify-content-end project-title '>
+                        <Col xl={3} lg={3} md={3} sm={3} xs={4}>
+                            <p className='project-title font-weight-bold'>Projects</p>
+                        </Col>
+                        
                     </Row>
                     
                     <Row className='d-flex flex-wrap align-items-center justify-content-center'> 
@@ -28,10 +32,9 @@ class Projects extends React.Component {
                                 <img className='repo-icon' src={GitHubIcon} alt='GitHub Icon'/>
                                 </a>
                                 <Badge variant='light'>HTML</Badge>
-                                <Badge variant='light'>Bootstrap</Badge>
                                 <Badge variant='light'>React</Badge>
+                                <Badge variant='light'>Bootstrap</Badge>
                             </Row>
-                            
                         </div>
                         <div className='project-tile'>
                             <a href='https://deonchoi.github.io/random-quote-machine/' target='_blank' rel="noopener noreferrer">
@@ -45,20 +48,22 @@ class Projects extends React.Component {
                                 <Badge variant='light'>HTML</Badge>
                                 <Badge variant='light'>CSS</Badge>
                                 <Badge variant='light'>React</Badge>
+                                <Badge variant='light'>Bootstrap</Badge>
                             </Row>
                         </div>
                         <div className='project-tile'>
-                            <a href='https://deonchoi.github.io/joke-cards' target='_blank' rel="noopener noreferrer">
-                                <img className='project-image ' src={JokeCardsImage} alt='Third project'/>
+                            <a href='https://deonchoi.github.io/tic-tac-toe' target='_blank' rel="noopener noreferrer">
+                                <img className='project-image ' src={TicTacToeImage} alt='Third project'/>
                             </a>
-                            <p className='font-weight-bold text-center'>Random Quote Machine</p>
+                            <p className='font-weight-bold text-center'>Tic-Tac-Toe</p>
                             <Row className='d-flex flex-wrap align-items-center justify-content-center'>
-                                <a href='https://github.com/DeonChoi/joke-cards' target='_blank' rel="noopener noreferrer">
+                                <a href='https://github.com/DeonChoi/tic-tac-toe' target='_blank' rel="noopener noreferrer">
                                 <img className='repo-icon' src={GitHubIcon} alt='GitHub Icon'/>
                                 </a>
+                                <Badge variant='light'>HTML</Badge>
+                                <Badge variant='light'>CSS</Badge>
                                 <Badge variant='light'>React</Badge>
                                 <Badge variant='light'>Bootstrap</Badge>
-                                <Badge variant='light'>HTML</Badge>
                             </Row>
                         </div>
                         <div className='project-tile'>
@@ -70,14 +75,15 @@ class Projects extends React.Component {
                                 <a href='https://github.com/DeonChoi/joke-cards' target='_blank' rel="noopener noreferrer">
                                 <img className='repo-icon' src={GitHubIcon} alt='GitHub Icon'/>
                                 </a>
+                                <Badge variant='light'>HTML</Badge>
+                                <Badge variant='light'>CSS</Badge>
                                 <Badge variant='light'>React</Badge>
                                 <Badge variant='light'>Bootstrap</Badge>
-                                <Badge variant='light'>HTML</Badge>
                             </Row>
                         </div>
                     </Row>
-                </Row>
-            </Col>
+                </Col>
+            </Row>
         );  
     }
 }
