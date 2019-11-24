1f64f13f99f8d8158e449f5911771fe23e83320a
Deon Choi <deon.choi@gmail.com>
2019-11-23 21:54:42 -0800

    updated Contact.js and About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ import JavaScriptIcon from '../images/AboutComponent/javascript.png';
 import jQueryIcon from '../images/AboutComponent/jquery.png';
 import ReactIcon from '../images/AboutComponent/react.png';
 import BootStrap4Icon from '../images/AboutComponent/bootstrap.png';
-import ReduxIcon from '../images/AboutComponent/redux.png';
+// import ReduxIcon from '../images/AboutComponent/redux.png';
 import DodgersIcon from '../images/AboutComponent/dodgers.png';
 
 class About extends React.Component {
@@ -23,7 +23,7 @@ class About extends React.Component {
                         I graduated from <a className='about-link' href='https://www.gwu.edu/' target='_blank' rel="noopener noreferrer">The George Washington University</a> in May 2019 with a Bachelor of Science in Statistics & a Minor in Computer Science. 
                         Shortly after graduation, I decided to dive into the world of web development.
                         <br></br>
-                        I enjoy creating most of my applications using React and Bootstrap and my goal is to master the <a className='about-link' href='https://www.educative.io/edpresso/what-is-mern-stack' target='_blank' rel="noopener noreferrer">MERN</a> stack.
+                        I enjoy creating most of my applications using React.js and Bootstrap and because my goal is to master the <a className='about-link' href='https://www.educative.io/edpresso/what-is-mern-stack' target='_blank' rel="noopener noreferrer">MERN</a> stack, I am currently learning to use Node.js, Express.js, and MongoDB.
                         I still have an enormous amount to learn, but I'm incredibly excited to continue down this path into becoming an excellent developer.
                         <br></br>
                         Aside from web development, I enjoy fitness, food, watching, and playing sports. I'm a Lakers, Rams/Chargers, and die-hard Dodgers fan - I know it's been a disappointing decade, please don't remind me.
