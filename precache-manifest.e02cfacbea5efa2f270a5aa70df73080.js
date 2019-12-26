12daf4b3cf83ab2439308c82d4ab97f8d4165fda
Deon Choi <deon.choi@gmail.com>
2019-12-25 18:18:25 -0800

    Updates

---
+++ precache-manifest.e02cfacbea5efa2f270a5aa70df73080.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45fe844f45cd09d8b851811eb047f3dc",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "7ffcdf0a24a224716809",
+    "url": "/my-portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c5fd69af1eedd3915f08",
+    "url": "/my-portfolio/static/css/main.9437d5ce.chunk.css"
+  },
+  {
+    "revision": "7ffcdf0a24a224716809",
+    "url": "/my-portfolio/static/js/2.51d23390.chunk.js"
+  },
+  {
+    "revision": "c5fd69af1eedd3915f08",
+    "url": "/my-portfolio/static/js/main.54ded1b1.chunk.js"
+  },
+  {
+    "revision": "2ab7c54528df7e89e2e7",
+    "url": "/my-portfolio/static/js/runtime-main.9fa63408.js"
+  },
+  {
+    "revision": "4ff14c1cecc7ce009fe6f96ddbda08c4",
+    "url": "/my-portfolio/static/media/Delish.4ff14c1c.png"
+  },
+  {
+    "revision": "958537cfa3b3ffb3eee052a7e4a0d1b7",
+    "url": "/my-portfolio/static/media/DeonChoiResume.958537cf.png"
+  },
+  {
+    "revision": "b611cdc9854004814b22f1c442e997c7",
+    "url": "/my-portfolio/static/media/Flix.b611cdc9.png"
+  },
+  {
+    "revision": "7cb318fb3d697c09287bed331bb06204",
+    "url": "/my-portfolio/static/media/PhraseHunter.7cb318fb.png"
+  },
+  {
+    "revision": "6748e5eab742c57a76ca4e1da2e9c716",
+    "url": "/my-portfolio/static/media/ShrinkURL.6748e5ea.png"
+  },
+  {
+    "revision": "584d9f322f08684db8ff2217731e474f",
+    "url": "/my-portfolio/static/media/bootstrap.584d9f32.png"
+  },
+  {
+    "revision": "a63bfccdd9a84243a578deaf72fd2a6e",
+    "url": "/my-portfolio/static/media/cherryblossomsdc.a63bfccd.jpg"
+  },
+  {
+    "revision": "c9421be52fe48fdf04839b53f8e9f1d4",
+    "url": "/my-portfolio/static/media/css3.c9421be5.png"
+  },
+  {
+    "revision": "3d4f7cd8a59bfce471f7431385b3935c",
+    "url": "/my-portfolio/static/media/dodgers.3d4f7cd8.png"
+  },
+  {
+    "revision": "d638bd34b425dec39792d4a81189eddf",
+    "url": "/my-portfolio/static/media/expressJS.d638bd34.png"
+  },
+  {
+    "revision": "56fe8aa902f49bead518a9c6439325b4",
+    "url": "/my-portfolio/static/media/gmail.56fe8aa9.png"
+  },
+  {
+    "revision": "2d693542b8f48896f17cc45c8e67c694",
+    "url": "/my-portfolio/static/media/goldengatebridge.2d693542.jpg"
+  },
+  {
+    "revision": "be84832018b380be2a8444313dc5427d",
+    "url": "/my-portfolio/static/media/html5.be848320.png"
+  },
+  {
+    "revision": "e8c27f6b0fcf596a06a31135d937c73e",
+    "url": "/my-portfolio/static/media/javascript.e8c27f6b.png"
+  },
+  {
+    "revision": "9999c1a8c347b81c5cb5df967664c4c3",
+    "url": "/my-portfolio/static/media/jquery.9999c1a8.png"
+  },
+  {
+    "revision": "a7d17e7c8cca6665d64947e16058b034",
+    "url": "/my-portfolio/static/media/linkedin.a7d17e7c.png"
+  },
+  {
+    "revision": "ce8540156af5e6c1b72af8d9a6abf3cf",
+    "url": "/my-portfolio/static/media/mongoDB.ce854015.png"
+  },
+  {
+    "revision": "313acec2dec25346ce03acdc6b2a3ef5",
+    "url": "/my-portfolio/static/media/nodeJS.313acec2.png"
+  },
+  {
+    "revision": "4ccd607c949a4e8d0701d4c22f0b5609",
+    "url": "/my-portfolio/static/media/nyc_skyline.4ccd607c.jpg"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/my-portfolio/static/media/react.82a318cc.png"
+  },
+  {
+    "revision": "8dcb74d1d48875c57cd10d941ff61666",
+    "url": "/my-portfolio/static/media/sass.8dcb74d1.png"
+  }
+]);
